e439ac5ac9aefdd193695c9b4a76a592652eecac
wjhong1234 <wjhong@shaw.ca>
2017-02-19 20:54:17 -0700

    should work pls
    defensive

--- ApocStrategyDefensive.hs
+++ ApocStrategyDefensive.hs
@@ -21,112 +21,57 @@ import Checks
 
 defensive :: Chooser
 -- * Game state based on a normal play, which would be moving a knight.  
-defensive gameState Normal player =
-    let board = theBoard gameState in
-    let defensiveMoves = (orderDefensiveMoves board player (piecesAtRisk board player)) in
-    if (length defensiveMoves > 0) && (randomChance 0.7) then
-      let move = chooseRandomMove defensiveMoves 0.75 in
-      if move /= Nothing then
-          return $ Just [(fst (fromJust move)), (snd (fromJust move))]
-      else return Nothing
-    else
-      let captureKnightPlay = (orderCapturePlay board player (allPiecesOfType board player "Knight"))
-          capturePawnPlay = (orderCapturePlay board player (allPiecesOfType board player "Pawn"))
-          noCapturePlay = (orderCapturePlay board player (allPieces board player))
-          move = chooseRandomMove (captureKnightPlay ++ capturePawnPlay ++ noCapturePlay) 0.75 in
-      if move /= Nothing then
-          return $ Just [(fst (fromJust move)), (snd (fromJust move))]
-      else return Nothing
+defensive board Normal White =
+    let move = chooseRandomMove ((validMoves board knightOffset (getPieces board White WK 0 0 0 0 4 4)) ++ (validMoves board White pawnOffset (getPieces board WP 0 0 0 0 4 4))) in
+    
+    if (move == Nothing)
+       then return Nothing
+       else return $ Just [(fromJust move)]
       
+defensive board Normal Black =
+    let move = chooseRandomMove ((validMoves board Black knightOffset (getPieces board BK 0 0 0 0 4 4)) ++ (validMoves board Black pawnOffset (getPieces board BP 0 0 0 0 4 4))) in
+    
+    if (move == Nothing)
+       then return Nothing
+       else return $ Just [(fromJust move)]      
+    
 -- * Game state based on a pawn move
-defensive gameState PawnPlacement player =
-  let emptyPieces = safeSquares (theBoard gameState) player (pieces (theBoard gameState) E 0 0 0 1 4 3) in
-  let move = chooseMove emptyPieces in
-  if move /= Nothing then
-    return $ Just [(fromJust move)]
-  else return Nothing
+defensive board PawnPlacement player =
+     let move = chooseRandomMove (getPieces board E 0 0 0 1 4 3)
+     
+     if (move == Nothing)
+        then return Nothing
+        else return $ Just [(fromJust move)]
 
-{- Move options
--}
+validMoves :: GameState -> Player -> [(Int, Int)] -> [(Int, Int)] -> [(Int, Int)]
+validMoves board player [] _ = []
+validMoves board player _ [] = []
+validMoves board player offsets (op:ops) = (goodMoves board player offsets op) ++ (validMoves board player offsets ops)
 
+goodMoves :: GameState -> Player -> [(Int, Int)] -> (Int, Int) -> [(Int, Int)]
+goodMoves board player (off:offs) op | (check == WP) && (player == White) = goodMoves board player offs op
+                                     | (check == WK) && (player == White) = goodMoves board player offs op
+                                     | (check == BP) && (player == Black) = goodMoves board player offs op
+                                     | (check == BK) && (player == Black) = goodMoves board player offs op
+                                     | otherwise = move : goodMoves board player offs op
+                                     where move = (addPair op off)
+                                           check = (getFromBoard (theBoard board) move)
 
-{- Plays that do not end in a capture
--}
+getPieces :: GameState -> Cell -> Int -> Int -> Int -> Int -> Int -> Int -> [(Int, Int)]
+getPieces board c x y xmin ymin xmax ymax | (x == xmax && y == ymax) = if ((getFromBoard (theBoard board)) (xmax, ymax)) == c 
+                                                                       then (x, y) : [] 
+                                                                       else []
+                                       | (x == xmax) = if ((getFromBoard (theBoard board)) (xmax, y)) == p 
+                                                          then (xmax, y) : getPieces board c xmin (y + 1) xmin ymin xmax ymax 
+                                                          else getPieces board c xmin (y + 1) xmin ymin xmax ymax
+                                       | otherwise = if ((getFromBoard (theBoard board)) (x, y)) == p 
+                                                        then (x, y) : getPieces board c (x + 1) y xmin ymin xmax ymax 
+                                                        else getPieces board c (x + 1) y xmin ymin xmax ymax
 
--- |Returns a list of the best no capture plays available on the board
-noCaptureMove :: Board -> Player -> [(Int, Int)] -> [((Int, Int), (Int, Int))]
-noCaptureMove b player [] = []
-noCaptureMove b player (p:ps) | (best == Nothing) = (noCaptureMove b player ps)
-                                  | otherwise = (p, fromJust best) : (noCaptureMove b player ps)
-                                  where movesList = allNotKills b p
-                                        best = chooseRandomMove (orderNoCapture b player movesList) 0.75
+addPair :: (Int, Int) -> (Int, Int) -> (Int, Int)
+addPair one two = ((fst one + fst two), (snd one + snd two))
 
--- Returns a list of no capture plays
-orderNoCapture :: Board -> Player -> [(Int, Int)] -> [(Int, Int)]
-orderNoCapture b player [] = []
-orderNoCapture b player (p:ps) | isSafePosition b player p = p : (orderNoCapture b player ps)
-                              | otherwise = (orderNoCapture b player ps) ++ [p]
-
-
--- | Returns a list of moves that will take a piece at risk and make it safe
-orderDefensiveMoves :: Board -> Player -> [(Int, Int)] -> [((Int, Int), (Int, Int))]
-orderDefensiveMoves b player [] = []
-orderDefensiveMoves b player (p:ps) | (bestMove == Nothing) = orderDefensiveMoves b player ps
-                                  | otherwise = (p, fromJust bestMove) : orderDefensiveMoves b player ps
-                                  where movesList = getMoves b p True
-                                        bestMove = chooseRandomMove(orderDefense b player movesList) 0.75
-
-
--- | Returns a list of safe places we are able to reach on the board
-orderDefense :: Board -> Player -> [(Int, Int)] -> [(Int, Int)]
-orderDefense b player [] = []
-orderDefense b player (m:ms) | not (safeTile b player m) = m : orderDefense b player ms
-                           | otherwise = orderDefense b player ms
-
-
-{- Plays that end in a capture
--}
-
--- |Returns a list of the best moves possible which will capture an eneemy piece
--- 
-orderCapturePlay :: Board -> Player -> [(Int, Int)] -> [((Int, Int), (Int, Int))]
-orderCapturePlay b player [] = []
-orderCapturePlay b player (p:ps) | (bestKillPiece == Nothing) = (orderCapturePlay b player ps)
-                        | (isSafePosition b player (fromJust bestKill)) = [(p, fromJust bestKill)] ++ orderCapturePlay b player ps
-                        | (fromJust bestKillPiece == Knight) = orderCapturePlay b player ps ++ [(p, fromJust bestKill)]
-                        | (fromJust bestKillPiece == Pawn)   = orderCapturePlay b player ps ++ [(p, fromJust bestKill)]
-                        where killsList = allKills b p
-                              bestKill = chooseRandomMove (orderCapture b player killsList) 0.75
-                              bestKillPiece | (bestKill == Nothing) = Nothing
-                                            | otherwise = Just $ typeOf (pieceOf (getFromBoard b (fromJust bestKill)))
-
-
--- |Return a list of pieces able to be capture in order of importance (Knight, Pawn)
-orderCapture :: Board -> Player -> [(Int, Int)] -> [(Int, Int)]
-orderCapture board player [] = []
-orderCapture board player (k:ks) | safeTile board player k = k : (orderCapture board player ks)
-                        | (piece == Knight) = (orderCapture board player ks) ++ [k]
-                        | (piece == Pawn)   = (orderCapture board player ks) ++ [k]
-                        where piece = typeOf (pieceOf (getFromBoard board k))
-
-
-{- Pieces in danger and safe spots
--}
-
--- |Returns a list of the current safe spaces on the board 
-safeSquares :: Board -> Player -> [(Int, Int)] -> [(Int, Int)]
-safeSquares b player [] = []
-safeSquares b player (x:xs) | (safeTile b player x) = x : safeSquares b player xs
-                              | otherwise = safeSquares b player xs
-
-
--- |Returns a list of pieces in danger on the board
-piecesAtRisk :: Board -> Player -> [(Int, Int)]
-piecesAtRisk b player = allPiecesAtRisk b player ((allPiecesOfType b player "Pawn") ++ (allPiecesOfType b player "Knight"))
-
-
--- |This method returns a list of pieces in danger of being captured
-allPiecesAtRisk :: Board -> Player -> [(Int, Int)] -> [(Int, Int)]
-allPiecesAtRisk b player [] = []
-allPiecesAtRisk b player (x:xs) | not (safeTile b player x) = x : allPiecesAtRisk b player xs
-                                  | otherwise = allPiecesAtRisk b player xs
+chooseRandomMove :: [(Int, Int)] -> Maybe (Int, Int)
+chooseRandomMove [] = Nothing
+chooseRandomMove ops = let i = unsafePerformIO(getStdRandom(randomR(0, length ops))) in
+                               Just(ops !! i)
